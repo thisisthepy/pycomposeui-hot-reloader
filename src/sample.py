681b82bc97b6c9df99ca7da1d47661801736523b
JUNU <96416870+MelonChicken@users.noreply.github.com>
2024-04-07 17:03:03 +0900

    Test: Test For Use as Module

---
+++ src/sample.py
@@ -0,0 +1,4 @@
+from src.main.server import run_server
+
+run_server(DIRECTORY_TO_WATCH=r"C:\Users\lenovo\OneDrive\문서\GitHub\PyComposeHotReloader\src\test\app\src",
+           ZIPFILE_DIR=r"C:\Users\lenovo\OneDrive\문서\GitHub\PyComposeHotReloader\src\test\app")
