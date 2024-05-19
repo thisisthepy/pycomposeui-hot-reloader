68264ff28d8f6bb5b1dcddeb9aa9514d438752f9
JUNU <96416870+MelonChicken@users.noreply.github.com>
2024-05-19 11:41:25 +0900

    Feat: Add Config File

--- src/main/server/__init__.py
+++ src/main/server/__init__.py
@@ -1,12 +1,20 @@
-import sys
 import os
+import sys
+import toml
 from os.path import abspath
-
 from ..server.main import run_server
-watch_dirs = []
-zip_dir = None
 
+script_path = os.path.dirname(__file__)
+config_path = abspath(os.path.join(script_path, '../../../config.toml'))
 
+with open(config_path, 'r') as f:
+    settings_toml = toml.load(f)
+
+watch_dirs = settings_toml['user']['directories_to_watch']
+zipfile_dir = settings_toml['user']['zipfile_dir']
+exception_dir = settings_toml['user']['exception_dir']
+host = settings_toml['server']['host']
+port = settings_toml['server']['port']
 for index, arg in enumerate(sys.argv):
     if arg == "-w":
         watch_dirs.append(abspath(sys.argv[index+1]))
@@ -15,7 +23,7 @@ for index, arg in enumerate(sys.argv):
 
 if len(watch_dirs) == 0:
     watch_dirs.append(os.getcwd())
-    if zip_dir == None:
-        zip_dir = os.path.abspath(os.path.join(watch_dirs[0], os.pardir))
+    if not zipfile_dir:
+        zipfile_dir = os.path.abspath(os.path.join(watch_dirs[0], os.pardir))
 
-run_server(directories_to_watch=watch_dirs, zipfile_dir=zip_dir, os_named=False)
+run_server(directories_to_watch=watch_dirs, zipfile_dir=zipfile_dir, os_named=False, host=host, port=port)
