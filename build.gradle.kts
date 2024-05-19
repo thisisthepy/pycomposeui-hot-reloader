229f63184bbd840b3aa896ddcd786f528f701c4d
JUNU <96416870+MelonChicken@users.noreply.github.com>
2024-05-19 19:12:08 +0900

    Remove: Delete Dependencies No Idea

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,31 +16,22 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-core:2.0.0")
-                implementation("io.ktor:ktor-client-websockets:2.0.0")
-                implementation("io.ktor:ktor-client-serialization:2.0.0")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
             }
         }
         val androidMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-okhttp:2.0.0") // Replace with appropriate client
             }
         }
         val iosMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-ios:2.0.0")
             }
         }
         val windowsMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-cio:2.0.0")
             }
         }
         val mingwMain by getting {
             dependencies {
-                implementation("io.ktor:ktor-client-cio:2.0.0")
             }
         }
     }
