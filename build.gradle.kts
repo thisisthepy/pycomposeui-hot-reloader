f659292fd09e91a108da11d75026d4f11d3ed750
JUNU <96416870+MelonChicken@users.noreply.github.com>
2024-05-19 19:03:34 +0900

    Feat: Build KMP Structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,47 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("multiplatform") version "1.8.0"
+    id("com.android.library") version "7.2.1"
+    kotlin("plugin.serialization") version "1.8.0"
 }
 
 group = "thisisthepy"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 kotlin {
-    jvmToolchain(21)
+    android()
+    ios()
+    jvm("windows")
+    mingwX64("mingw")
 
     sourceSets {
-        val main by getting {
-            kotlin.srcDirs("src/main")
-            resources.srcDirs("res")
+        val commonMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-core:2.0.0")
+                implementation("io.ktor:ktor-client-websockets:2.0.0")
+                implementation("io.ktor:ktor-client-serialization:2.0.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-okhttp:2.0.0") // Replace with appropriate client
+            }
+        }
+        val iosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:2.0.0")
+            }
+        }
+        val windowsMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-cio:2.0.0")
+            }
+        }
+        val mingwMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-cio:2.0.0")
+            }
         }
     }
 }
---
+++ src/main/client/mingwMain/kotlin/mingwMain.kt
@@ -0,0 +1,2 @@
+package client.mingwMain.kotlin
+
